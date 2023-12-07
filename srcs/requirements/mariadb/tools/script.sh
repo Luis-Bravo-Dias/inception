0dc169014a29ec67652c56b8e80e19075062b98d
Luis Bravo Dias <luisbravodias@sapo.pt>
2023-12-07 20:36:42 +0000

    MARIADB AAAAAAAAAAAAAAA

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 service mysql start;
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
@@ -6,5 +8,5 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
