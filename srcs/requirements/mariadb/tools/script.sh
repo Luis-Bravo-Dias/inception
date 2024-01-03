60f6b1042037f8be35821d0e82bf35e08ac8f1d1
luis <luis@debian.lleiria>
2024-01-03 11:20:29 -0600

    8

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,6 +2,8 @@
 
 service mysql start;
 
+sleep 3;
+
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
@@ -10,3 +12,24 @@ mysql -u root -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
+
+if [ ! -d "/var/lib/mysql/$SQL_DATABASE" ]; then
+
+service mysql start;
+
+# sleep 3;
+
+
+# echo "Creating admin user"
+# mysql -u root -e "CREATE DATABASE $SQL_DATABASE;"
+# mysql -u root -e "CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%';"
+# mysql -u root -e "FLUSH PRIVILEGES;"
+
+# echo "Setting password for root user"
+# mysqladmin -u root password "$SQL_PASSWORD"
+
+# mysqladmin -u root -p"$SQL_PASSWORD" shutdown
+# fi
+
+# exec mysqld_safe --bind-address=0.0.0.0
